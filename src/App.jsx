c3cf78d295ec030e0a372bfeb2f41d2d5ba2c56c
john.mukenge <john.mukenge@emeal.nttdata.com>
2024-11-25 21:23:24 +0100

    Workflow section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import FeatureSection from "./components/FeatureSection"
 import HeroSection from "./components/HeroSection"
 import Navbar from "./components/Navbar"
+import Workflow from "./components/Workflow"
 
 function App() {
 
@@ -9,7 +10,8 @@ function App() {
       <Navbar />
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
-        <FeatureSection />  
+        <FeatureSection />
+        <Workflow />
       </div>
       
     </>
