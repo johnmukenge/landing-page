663bfe20da4d4eedeae6f6aee78564fe9e2ddda0
john.mukenge <john.mukenge@emeal.nttdata.com>
2024-11-25 21:56:34 +0100

    Testimonials section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import FeatureSection from "./components/FeatureSection"
 import HeroSection from "./components/HeroSection"
 import Navbar from "./components/Navbar"
 import Price from "./components/Price"
+import Testimonials from "./components/Testimonials"
 import Workflow from "./components/Workflow"
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <FeatureSection />
         <Workflow />
         <Price />
+        <Testimonials />
       </div>
       
     </>
