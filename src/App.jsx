c7701c4308a1011c7e1447f9cb923648516afcb9
john.mukenge <john.mukenge@emeal.nttdata.com>
2024-11-25 21:43:04 +0100

    Pricing section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import FeatureSection from "./components/FeatureSection"
 import HeroSection from "./components/HeroSection"
 import Navbar from "./components/Navbar"
+import Price from "./components/Price"
 import Workflow from "./components/Workflow"
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <HeroSection />
         <FeatureSection />
         <Workflow />
+        <Price />
       </div>
       
     </>
